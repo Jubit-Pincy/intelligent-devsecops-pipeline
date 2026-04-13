d16cdd6eeadda9ea891a3067a9a4726492ac81fb
Jubit-Pincy <jubitpincy@gmail.com>
2026-04-13 06:46:41 +0000

    Corrected syntax in weather forecast controller

--- App/Controllers/WeatherForecastController.cs
+++ App/Controllers/WeatherForecastController.cs
@@ -7,28 +7,22 @@ namespace App.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        [HttpGet("health")]
-        public IActionResult HealthCheck()
+    [HttpGet("health")]
+    [ProducesResponseType(typeof(object), StatusCodes.Status200OK)] 
+    public IActionResult HealthCheck()
+    {
+        return Ok(new { status = "Application is running" });
+    }
+    
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        return Ok(new
         {
-            return Ok(new { status = "Application is running" });
-        }
-
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] LoginRequest request)
-        {
-            
-            return Ok(new
-            {
-                message = "Login endpoint hit",
-                user = request.Username
-            });
-        }
-        [HttpGet("health")]
-        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)] 
-        public IActionResult HealthCheck() { ... }
-
-        [HttpPost("login")]
-        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)] 
-        public IActionResult Login([FromBody] LoginRequest request) { ... }
+            message = "Login endpoint hit",
+            user = request.Username
+        });
+    }
     }
 }
